279b3687a2e8d6196928d520041de441ec9b22d5
vbyoshara <vbyoshara@gmail.com>
2019-08-28 00:36:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/student/baitapandroid_congphamquocviet_16023091_dhktpm12att/MainActivity_Bai4.java
@@ -0,0 +1,19 @@
+package com.example.student.baitapandroid_congphamquocviet_16023091_dhktpm12att;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity_Bai4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__bai4);
+    }
+
+    public void checked(View view) {
+    }
+}
